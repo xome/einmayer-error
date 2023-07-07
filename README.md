533c09e8a2762fc296fc92d4ebddf9ef3e5da007
Johannes Mayer <johannes@einmayer.de>
2023-07-07 14:13:56 +0200

    genereller Fehler (erstmal auf 500 betitelt). Mailversand an webmaster.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # einmayer-error
 Error Pages for private webserver
+
+## Setup
+Create file public/Constants.php with an example content:
+
+```php
+<?php
+
+class Constants {
+
+    public const WEBMASTER_MAIL = "example@example.org";
+    public const DOMAIN = "example.org";
+
+}
+```
---
+++ public/500.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fehler bei Verarbeitung</title>
+</head>
+<body>
+
+<p>Entschuldigung, da ist ein Fehler passiert.<br>
+Der Admin wurde kontaktiert.<br>
+Es gibt keinen Handlungsbedarf.</p>
+
+<p>Sorry, there has been an error.<br>
+The admin has been contacted.<br>
+There is nothing to do.</p>
+
+<?php
+
+require_once "Constants.php";
+
+$message = 'Woops! Da ist ein Fehler bei einmayer.de passiert:' . PHP_EOL .
+    'PID: ' . getmypid() . PHP_EOL .
+    'Remote Address: ' . $_SERVER['REMOTE_ADDR'] . PHP_EOL .
+    'Date: ' . date('c') . PHP_EOL .
+    'Last Error: ' . error_get_last()["message"];
+
+mail(Constants::WEBMASTER_MAIL,
+    'Laufzeitfehler auf ' . Constants::DOMAIN,
+    $message) ?>
+</body>
+</html>
